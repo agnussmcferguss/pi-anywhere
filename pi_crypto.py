a90ea8f815842d0228a5ac8238f3a1343ed92064
Chris <agnussmcferguss@hotmail.com>
2016-07-10 23:18:27 +0930

    updated crypto

--- pi_crypto.py
+++ pi_crypto.py
@@ -1,6 +1,7 @@
 # Encryption stub - possibly unnecessary duplication
-
 import rsa
+import cPickle as pickle
+import sys
 
 def create_key_pair(size=2048) :
 	pubkey, privkey = rsa.newkeys(size)
@@ -11,3 +12,9 @@ def encrypt_message(message, pubkey) :
 
 def decrypt_message(message, privkey) :
 	return rsa.decrypt(message, privkey)
+
+if __name__ == '__main__' :
+	message = sys.argv[1]
+	with open('./pubkey.key.pkl') as f :
+		pubkey = pickle.load(f)
+	print encrypt_message(message, pubkey)
